78f683e6aeeaa429257d5b44dc2a179129862bb4
jaywcjlove <398188662@qq.com>
2018-09-16 21:40:04 +0800

    Modify the compilation tool.

--- script/deploy.js
+++ script/deploy.js
@@ -4,7 +4,7 @@ const loading = require('loading-cli');
 require('colors-cli/toxic');
 const pkg = require('../package.json');
 
-const ORIGIN = 'https://github.com/uiw-react/icons.git';
+const ORIGIN = "git@github.com:uiw-react/icons.git";
 const BRANCH = 'gh-pages';
 
 console.log('  Start public to your git repo'.green);
@@ -16,14 +16,18 @@ const load = loading({
   stream: process.stdout,
 }).start();
 
-ghpages.publish(path.resolve(path.join(process.cwd(), 'fonts')), {
-  branch: BRANCH,
-  repo: ORIGIN,
-  message: `Update uiw-iconfont v${pkg.version} document., ${new Date()}!`,
-}, (err) => {
-  load.stop();
-  if (err) {
-    return console.log(err);
+ghpages.publish(
+  path.resolve(path.join(process.cwd(), "fonts")),
+  {
+    branch: BRANCH,
+    repo: ORIGIN,
+    message: `Update uiw-iconfont v${pkg.version} document. ${new Date()}!`
+  },
+  err => {
+    load.stop();
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`\n Push to ${BRANCH} success!\n`.green.bold);
   }
-  console.log(`\n  Push to ${BRANCH} success!\n`.green.bold);
-});
+);
---
+++ script/index.js
@@ -0,0 +1,58 @@
+const svgtofont = require("svgtofont");
+const path = require("path");
+const pkg = require('../package.json')
+
+svgtofont({
+  src: path.resolve(process.cwd(), "icon"), // svg path
+  dist: path.resolve(process.cwd(), "fonts"), // output path
+  fontName: "w-iconfont", // font name
+  css: true, // Create CSS files.
+  startNumber: 20000, // unicode start number
+  nodemo: true, // no demo html files
+  svgicons2svgfont: {
+    fontHeight: 1000,
+    normalize: true
+  },
+  website: {
+    title: "uiw icons",
+    // Must be a .svg format image.
+    logo: path.resolve(process.cwd(), "icon", "uiw.svg"),
+    version: pkg.version,
+    meta: {
+      description: "The premium icon font for @uiw-react Component Library.",
+      keywords:
+        "uiw, uiw-react, react.js, react, component, svg, icon, components, ui, framework, toolkit,icon,file-icons,TTF,EOT,WOFF,WOFF2,SVG"
+    },
+    description: `
+      The premium icon font for <a href="https://github.com/uiw-react/uiw">@uiw-react</a> Component Library.
+    `,
+    links: [
+      {
+        title: "Cheatsheet",
+        url: "https://github.com/uiw-react/icons"
+      },
+      {
+        title: "GitHub",
+        url: "https://uiw-react.github.io/#/cn/icon"
+      },
+      {
+        title: "Feedback",
+        url: "https://github.com/uiw-react/icons/issues"
+      },
+      {
+        title: "Font Class",
+        url: "index.html"
+      },
+      {
+        title: "Unicode",
+        url: "unicode.html"
+      }
+    ],
+    footerInfo: `
+      Designed by <a href="https://github.com/liwen0526">@liwen0526</a>. 
+      Licensed under MIT. (Yes it's free and <a href="https://github.com/uiw-react/icons">open-sourced</a>)
+    `
+  }
+}).then(() => {
+  console.log("done!");
+});
