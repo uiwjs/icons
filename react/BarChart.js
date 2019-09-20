cb8d83a6d7546409eb9d07b43e044b7ed51fa14f
jaywcjlove <398188662@qq.com>
2019-09-20 22:54:19 +0800

    Updates

--- react/BarChart.js
+++ react/BarChart.js
@@ -2,5 +2,5 @@
 import React from 'react';
 
 export const BarChart = props => (
-  <svg viewBox="0 0 20 20" {...props}><path d="M1 6h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1zm5.365 6h2a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1zm5.332-12h2a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1zM17 0h2a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1z" fillRule="evenodd" /></svg>
+  <svg viewBox="0 0 20 20" {...props}><path d="M1 6h2a1 1 0 011 1v12a1 1 0 01-1 1H1a1 1 0 01-1-1V7a1 1 0 011-1zm5.365 6h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6a1 1 0 011-1zm5.332-12h2a1 1 0 011 1v18a1 1 0 01-1 1h-2a1 1 0 01-1-1V1a1 1 0 011-1zM17 0h2a1 1 0 011 1v18a1 1 0 01-1 1h-2a1 1 0 01-1-1V1a1 1 0 011-1z" fillRule="evenodd" /></svg>
 );
