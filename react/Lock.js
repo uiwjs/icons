e0956c569a440baccc206e1ffed6ab028b53ce1c
jaywcjlove <398188662@qq.com>
2019-11-05 11:38:48 +0800

    Update uiw-iconfont v2.2.2 document. Tue Nov 05 2019 11:38:24 GMT+0800 (GMT+08:00)!

--- react/Lock.js
+++ react/Lock.js
@@ -2,5 +2,5 @@
 import React from 'react';
 
 export const Lock = props => (
-  <svg viewBox="0 0 20 20" {...props}><path d="M16.6 10.245H3.4V18.6h13.2v-8.355zm0-1.4V8A6.6 6.6 0 003.4 8v.845h13.2zM10 0a8 8 0 018 8v11a1 1 0 01-1 1H3a1 1 0 01-1-1V8a8 8 0 018-8zm-.345 14.447a1.5 1.5 0 111.4.006v2.169a.7.7 0 01-1.4 0v-2.175z" fillRule="evenodd" /></svg>
+  <svg viewBox="0 0 20 20" {...props}><path d="M10 0a8 8 0 018 8v11a1 1 0 01-1 1H3a1 1 0 01-1-1V8a8 8 0 018-8zm6.6 10.245H3.4V18.6h13.2v-8.355zm-6.239 1.378a1.5 1.5 0 01.695 2.83v2.169a.7.7 0 01-1.4 0v-2.175a1.5 1.5 0 01.705-2.824zM10 1.4A6.6 6.6 0 003.4 8v.845h13.2V8A6.6 6.6 0 0010 1.4z" fillRule="evenodd" /></svg>
 );
