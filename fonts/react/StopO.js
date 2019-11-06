6f265a7de07f8dc3f4052585452f6bc3104558e5
jaywcjlove <398188662@qq.com>
2019-11-06 11:21:58 +0800

    released v2.3.0

---
+++ fonts/react/StopO.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+
+export const StopO = props => (
+  <svg viewBox="0 0 20 20" {...props}><path d="M10 0C4.478 0 0 4.478 0 10s4.478 10 10 10 10-4.478 10-10S15.522 0 10 0zm0 18.304A8.305 8.305 0 013.56 4.759l11.681 11.68A8.266 8.266 0 0110 18.305zm6.44-3.063L4.759 3.561a8.305 8.305 0 0111.68 11.68z" fillRule="evenodd" /></svg>
+);
